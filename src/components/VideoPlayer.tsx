038a41fc2282d879936d4bd6fae38d154c59b67b
Serg <sergey.gabrielyan1998@gmail.com>
2024-01-13 18:09:29 +0400

    #2 Change video file

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -4,7 +4,7 @@ import dynamic from "next/dynamic";
 const ReactPlayer = dynamic(() => import("react-player/lazy"), { ssr: false });
 
 //video path
-let videosrc = "https://bucket-viewer.s3.amazonaws.com/viewer1664370329252.mp4";
+let videosrc = "./videos/singapur.mp4";
 
 const VideoPlayer = () => {
   return (
